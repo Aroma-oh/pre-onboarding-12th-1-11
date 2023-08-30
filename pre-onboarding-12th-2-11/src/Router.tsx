80cd78753f254fa1875adbcffc5c5b3d8a959f97
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-30 09:55:19 +0900

    feat: router 설정

---
+++ pre-onboarding-12th-2-11/src/Router.tsx
@@ -0,0 +1,23 @@
+import App from 'App';
+import Detail from 'pages/Detail';
+import List from 'pages/List';
+import NotFound from 'pages/NotFound';
+import { createBrowserRouter } from 'react-router-dom';
+
+export const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/list',
+        element: <List />,
+      },
+      {
+        path: '/detail',
+        element: <Detail />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
